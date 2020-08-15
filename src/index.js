8436fa33b82e9d064d2df65868c60a45420b0454
kierra <51520037+kierrakay@users.noreply.github.com>
2020-08-15 15:41:27 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
+import { Provider } from 'react-redux'; /* code change */
 import shoppingListItemReducer from './reducers/shoppingListItemReducer';
 import App from './App';
 import './index.css';
-
+ 
 const store = createStore(
   shoppingListItemReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
-
+ 
 ReactDOM.render(
-  <App store={store}/>,
+  <Provider store={store}>
+    <App />
+  </Provider>, /* code change */
   document.getElementById('root')
 );
+
+//The Provider is a component that comes
+//  from our React Redux library. It wraps around 
+//  our App component. It does two things for us. 
+//  The first is that it will alert our Redux app
+//   when there has been a change in state, and this 
+//   will re-render our React app
